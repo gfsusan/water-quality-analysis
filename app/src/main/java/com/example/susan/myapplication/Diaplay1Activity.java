782a191896219dc434f1ee38453d4b8ed255170a
Susan Kim <gfsusan@naver.com>
2018-01-25 15:22:50 +0900

    Added necessary Activities
    1. Display1Activity.java
    2. activity_display1.xml
    3. Display2Activity.java
    4. activity_display2.xml

---
+++ app/src/main/java/com/example/susan/myapplication/Diaplay1Activity.java
@@ -0,0 +1,27 @@
+package com.example.susan.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class Diaplay1Activity extends AppCompatActivity {
+    Button buttonMove;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display1);
+        buttonMove = (Button)findViewById(R.id.buttonMove);
+
+        buttonMove.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClic(View v) {
+                Intent intent = new Intent(getApplicationContext(), Display2Activity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
