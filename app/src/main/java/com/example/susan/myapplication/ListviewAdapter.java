0c72939bcf7be94c8690a0d79c8c82208d2ca243
Susan Kim <gfsusan@naver.com>
2018-01-31 10:28:07 +0900

    리스트 뷰 만듦

---
+++ app/src/main/java/com/example/susan/myapplication/ListviewAdapter.java
@@ -0,0 +1,10 @@
+package com.example.susan.myapplication;
+
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Susan on 2018-01-31.
+ */
+
+public class ListviewAdapter extends BaseAdapter {
+}
