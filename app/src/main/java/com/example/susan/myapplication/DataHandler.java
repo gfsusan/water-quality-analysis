a47f14f578081f30d3008b4d12279afdc439d48c
Susan Kim <gfsusan@naver.com>
2018-02-02 08:40:30 +0900

    Created DataHandler class

---
+++ app/src/main/java/com/example/susan/myapplication/DataHandler.java
@@ -0,0 +1,134 @@
+package com.example.susan.myapplication;
+
+import android.content.ContentValues;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+/**
+ * Created by Susan on 2018-02-01.
+ */
+
+public class DataHandler extends AsyncTask<Void, Void, String> {
+    private String[][] data;
+    private String url;
+    private ContentValues values;
+    // location추리는거
+    private HashSet<Integer> location1IDs, location2IDs, location3IDs, location4IDs;
+
+    public DataHandler(String url, ContentValues values) {
+
+        this.url = url;
+        this.values = values;
+
+        String temp = "NJ-DS-CLI-01@나주 다시(배) 잔류염소@NJ-DS@금영섬@전남중부권@나주@남평@null@null@0.0@0@CLI@0.8@0.5@잔류염소@PPM<br>NJ-NP-CLI-01@나주 남평(배) 잔류염소@NJ-NP@금영섬@전남중부권@나주@남평@d3@180123-12:34@0.46@0@CLI@0.8@0.5@잔류염소@PPM<br>NJ-NP-PHI-01@나주 남평(배) pH@NJ-NP@금영섬@전남중부권@나주@남평@d1@180123-12:34@7.11@0@PHI@7.5@6.5@PH@PH<br>NJ-NP-TBI-01@나주 남평(배) 탁도@NJ-NP@금영섬@전남중부권@나주@남평@d4@180123-12:34@0.45@0@TBI@0.5@-9999.0@탁도@NTU<br>NJ-NP-TEI-01@나주 남평(배) 온도@NJ-NP@금영섬@전남중부권@나주@남평@d2@180123-12:34@10.1@0@TEI@30.0@4.0@온도@℃<br>WD-ND-PHI-01@완도 노두(배) pH@WD-ND@금영성@전남서남권@완도@대야@null@null@0.0@0@PHI@7.5@6.5@PH@PH<br>WD-ND-TEI-01@완도 노두(배) 온도@WD-ND@금영성@전남서남권@완도@대야@null@null@0.0@0@TEI@30.0@4.0@온도@℃";
+        System.out.print(temp);
+        String[] temp2 = temp.split("<br>");
+        data = new String[temp2.length][16];
+        for (int i = 0; i< temp2.length; i++ ) {
+            data[i] = new String[15];
+            data[i] = temp2[i].split("@");
+        }
+
+        location1IDs = new HashSet<>();
+        for(int i=0; i<data.length; i++)
+            location1IDs.add(i);
+    }
+
+    @Override
+    protected String doInBackground(Void... params) {
+
+        String result; // 요청 결과를 저장할 변수.
+        RequestHttpURLConnection requestHttpURLConnection = new RequestHttpURLConnection();
+        result = requestHttpURLConnection.request(url, values); // 해당 URL로 부터 결과물을 얻어온다.
+
+        return result;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+
+        //doInBackground()로 부터 리턴된 값이 onPostExecute()의 매개변수로 넘어오므로 s를 출력한다.
+        // TODO 이거 주석처리하면 원래랑 똑같이 실행됨
+//        String[] lines = s.split("<br>");
+
+        System.out.print(s);
+
+//        data = new String[lines.length][15];
+//        for(int i = 0; i < lines.length; i++){
+//            data[i] = lines[i].split("@");
+//        }
+
+
+
+    }
+
+    public ArrayList<String> getNextLocationCandidate() {
+        HashSet<String> hashSet = new HashSet<>();
+
+        for (int i:location1IDs) {
+            hashSet.add(data[i][3]);
+        }
+        ArrayList<String> location1s = new ArrayList<>();
+        location1s.add(0, "선택");
+        location1s.addAll(hashSet);
+
+        return location1s;
+    }
+
+    public ArrayList<String> getNextLocationCandidate(String location1) {
+        HashSet<String> hashSet = new HashSet<>();
+        location2IDs = new HashSet<>();
+        for(int i: location1IDs) {
+            if(data[i][3].equals(location1)) {
+                hashSet.add(data[i][4]);
+                location2IDs.add(i);
+            }
+        }
+        ArrayList<String> location2s = new ArrayList<>();
+        location2s.add(0,"선택");
+        location2s.addAll(hashSet);
+
+        return location2s;
+    }
+
+    public ArrayList<String> getNextLocationCandidate(String location1, String location2) {
+        HashSet<String> hashSet = new HashSet<>();
+        location3IDs = new HashSet<>();
+        for(int i: location2IDs) {
+            if(data[i][3].equals(location1) && data[i][4].equals(location2)) {
+                hashSet.add(data[i][5]);
+                location3IDs.add(i);
+            }
+        }
+        ArrayList<String> location3s = new ArrayList<>();
+        location3s.add(0,"선택");
+        location3s.addAll(hashSet);
+
+        return location3s;
+    }
+
+    public ArrayList<String> getNextLocationCandidate(String location1, String location2, String location3) {
+        HashSet<String> hashSet = new HashSet<>();
+        location4IDs = new HashSet<>();
+        for(int i: location3IDs) {
+            if(data[i][3].equals(location1) && data[i][4].equals(location2) && data[i][5].equals(location3)) {
+                hashSet.add(data[i][6]);
+                location4IDs.add(i);
+            }
+        }
+        ArrayList<String> location4s = new ArrayList<>();
+        location4s.add(0,"선택");
+        location4s.addAll(hashSet);
+
+        return location4s;
+    }
+
+
+    public String[][] getData() {
+        return data;
+    }
+}
--- app/src/main/java/com/example/susan/myapplication/Display1Activity.java
+++ app/src/main/java/com/example/susan/myapplication/Display1Activity.java
@@ -1,13 +1,16 @@
 package com.example.susan.myapplication;
 
 import android.app.ActionBar;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Layout;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -23,12 +26,17 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.lang.reflect.Array;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Display1Activity extends AppCompatActivity {
+    DataHandler dh;
     ImageButton buttonBack;
     Button buttonMove;
     Spinner spinner1, spinner2, spinner3, spinner4;
@@ -37,13 +45,25 @@ public class Display1Activity extends AppCompatActivity {
     LinearLayout verticalLayout;
     ListView listView;
 
+    String location1, location2, location3, location4;
     final String nothing = "선택";
-    int count = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display1);
+
+        String url = "http://165.194.35.103:8181/helloWeb/HServlet";
+        dh = new DataHandler(url, null);
+        dh.execute();
+        String[][] data = dh.getData();
+//            for (String[] a : data) {
+//                for (String b : a) {
+//                    System.out.print(b);
+//                    Log.d(this.getClass().getName(), b);
+//                }
+//            }
         buttonBack = (ImageButton)findViewById(R.id.buttonBack);
         buttonMove = (Button)findViewById(R.id.buttonMove);
 
@@ -52,9 +72,8 @@ public class Display1Activity extends AppCompatActivity {
         spinner3 = (Spinner)findViewById(R.id.spinner3);
         spinner4 = (Spinner)findViewById(R.id.spinner4);
 
-//        tableLayout = (TableLayout)findViewById(R.id.tableLayout);
-//        TableLayout.LayoutParams lp = new TableLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         listView = (ListView)findViewById(R.id.listView);
+
 
         // defaultAdapter 생성
         defaultAdapter = new ArrayAdapter<String>(getApplicationContext(),
@@ -66,12 +85,10 @@ public class Display1Activity extends AppCompatActivity {
         spinner3.setAdapter(defaultAdapter);
         spinner4.setAdapter(defaultAdapter);
 
-
-
         // spinner1에게 arrayAdapter 줭
         spinner1.setAdapter(new ArrayAdapter<String>(getApplicationContext(),
                 R.layout.support_simple_spinner_dropdown_item,
-                makeSpinnerListFrom("http://asdfafds.com", 1)));
+                dh.getNextLocationCandidate()));
 
 
         // spinner1의 item이 선택되었을 때
@@ -79,6 +96,7 @@ public class Display1Activity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                     // 선택되면 DB에서 데이터 불러와서 spinner2에 item list 보여줌
+                location1 = spinner1.getSelectedItem().toString();
                 if(spinner1.getSelectedItemPosition()==0) {
                     spinner2.setAdapter(defaultAdapter);
                     spinner3.setAdapter(defaultAdapter);
@@ -87,7 +105,7 @@ public class Display1Activity extends AppCompatActivity {
                 else {
                     spinner2.setAdapter(new ArrayAdapter<String>(getApplicationContext(),
                             R.layout.support_simple_spinner_dropdown_item,
-                            makeSpinnerListFrom("http://asdfafds.com", 2)));
+                            dh.getNextLocationCandidate(location1)));
 
                     //하위 스피너 초기화
                     spinner3.setAdapter(defaultAdapter);
@@ -106,6 +124,7 @@ public class Display1Activity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                     // 선택되면 DB에서 데이터 불러와서 spinner2에 item list 보여줌
+                location2 = spinner2.getSelectedItem().toString();
                 if(spinner2.getSelectedItemPosition() == 0) {
                     spinner3.setAdapter(defaultAdapter);
                     spinner4.setAdapter(defaultAdapter);
@@ -113,7 +132,7 @@ public class Display1Activity extends AppCompatActivity {
                 else {
                     spinner3.setAdapter(new ArrayAdapter<String>(getApplicationContext(),
                             R.layout.support_simple_spinner_dropdown_item,
-                            makeSpinnerListFrom("http://asdfafds.com", 3)));
+                            dh.getNextLocationCandidate(location1, location2)));
 
                     // 하위 스피너 초기화
                     spinner4.setAdapter(defaultAdapter);
@@ -132,13 +151,14 @@ public class Display1Activity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                     // 선택되면 DB에서 데이터 불러와서 spinner2에 item list 보여줌
+                location3 = spinner3.getSelectedItem().toString();
                 if(spinner3.getSelectedItemPosition() == 0) {
                     spinner4.setAdapter(defaultAdapter);
                 }
                 else {
                     spinner4.setAdapter(new ArrayAdapter<String>(getApplicationContext(),
                             R.layout.support_simple_spinner_dropdown_item,
-                            makeSpinnerListFrom("http://asdfafds.com", 4)));
+                            dh.getNextLocationCandidate(location1, location2, location3)));
                 }
             }
 
@@ -154,6 +174,7 @@ public class Display1Activity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                     // 선택되면 화면중앙부분 text set
                 if(spinner4.getSelectedItemPosition() != 0) {
+                    //TODO dfsfafsdf
                     // 해당 station의 datapoint 개수를 받아온다.
                     int nDataPoint = 5;
                     double c1 = 5;
@@ -222,4 +243,5 @@ public class Display1Activity extends AppCompatActivity {
 
         return list;
     }
+
 }
